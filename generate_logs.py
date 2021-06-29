eeec11d401e45ecd4b7eed15761720aaf2ba5c9d
Animesh <er.animesh6@hotmail.com>
2021-06-29 21:46:59 +0530

    Initial commit.

--- generate_logs.py
+++ generate_logs.py
@@ -2,7 +2,7 @@ import logging
 
 
 def log():
-    logging.basicConfig(filename="/Users/animeshmukherjee/PycharmProjects/pythonProject/Utilities/logs/logfile.log",
+    logging.basicConfig(filename="../Utilities/logs/logfile.log",
                         format='%(asctime)s : %(levelname)s : %(message)s)',
                         datefmt='%m/%d/%Y %I:%M:%S %p',
                         level=logging.INFO
