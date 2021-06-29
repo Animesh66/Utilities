60c209207571328cc984b583dca725ac0cc931ee
Animesh <er.animesh6@hotmail.com>
2021-06-29 22:08:04 +0530

    Initial commit.

--- configReader.py
+++ configReader.py
@@ -3,7 +3,7 @@ from configparser import ConfigParser
 
 def readConfig(section, key):
     config = ConfigParser()
-    config.read("../configuration_data/config.ini")
+    config.read("../Utilities/configuration_data/config.ini")
     return config.get(section, key)
 
 
