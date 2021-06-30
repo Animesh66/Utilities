b79fcc23ef618aa0f2226f951d4b73dca6313e39
Animesh <er.animesh6@hotmail.com>
2021-06-30 16:03:55 +0530

    Database connection utility added.

--- database_connectivity/database_connectivity.py
+++ database_connectivity/database_connectivity.py
@@ -1,16 +1,20 @@
 import mysql.connector
 
-def create_db_connection:
 
-mydb = mysql.connector.connect(
-    host="localhost",  # IP address of the database server
-    user="root",
-    password="root",
-    database="sql_student"
-)
+def create_db_connection():
+    mydb = mysql.connector.connect(
+        host="127.0.0.1",  # IP address of the database server
+        user="root",
+        password="nitaI9093@",
+        database="sql_student"
+    )
+    mycursor = mydb.cursor()  # This command is to create the Database connection
+    mycursor.execute("INSERT INTO student VALUES(28, 'Tweety', 1)")
+    mycursor.execute("SELECT * FROM student")  # This command is going to create a data in
+    # the database table
+    results = mycursor.fetchall()  # the result data will be stored in the results variable
+    for result in results:
+        print(result)
 
-mycursor = mydb.cursor()
-mycursor.execute("INSERT INTO student VALUES(2, 'Tweety', NULL)")
-results =mycursor.fetchall()
-for result in results:
-    print(result)
+
+create_db_connection()
